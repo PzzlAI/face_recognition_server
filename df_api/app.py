c4d31f742990c0bd7d4eaf88b8264ec55537142e
ratatosck <45779848+ratatosck@users.noreply.github.com>
2022-01-04 12:57:33 -0500

    Update app.py
    added endpoints for clocking in (marcacion) and reading a collaborator's clock in history (leer marcaciones).

--- df_api/app.py
+++ df_api/app.py
@@ -5,6 +5,8 @@ import os
 import shutil
 from starlette.testclient import TestClient
 from pymongo import MongoClient
+import datetime
+from bson.json_util import dumps
 
 app = FastAPI()
 
@@ -57,11 +59,6 @@ async def recognize_person(company_code: str = Form(...), employee_code: str = F
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-
-    # respuesta del servidor
-    if(not df.empty):
-        print(df.head())
-
     try:
         if(not df.empty):
             print(df.head())
@@ -81,12 +78,83 @@ async def recognize_person(company_code: str = Form(...), employee_code: str = F
             return {"access": False, "status": "no similar faces found", "name": None}
         
     except Exception as e:
-        print(e)
-        return("error de inferencia")
+        return(e)
+
+
 
     # finally:
     #     if db and type(db)==MongoClient:
     #         db.close()
+
+@app.post('/marcacion')
+async def recognize_person(company_code: str = Form(...), employee_code: str = Form(...), file: UploadFile = File(...), latitude: str = Form(...), longitude: str = Form(...)):
+    print("recibiendo imagen")
+    image_path = "./db/" + company_code + "/" + employee_code
+
+    # guardar archivo como jpg en tmp folder
+    with open(f"{IMAGEDIR}image.jpg", "wb") as image:
+        shutil.copyfileobj(file.file, image)
+    
+    # encontrar imagenes parecidas en carpeta de imagenes
+    metrics = ["cosine", "euclidean", "euclidean_l2"]
+    models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib"]
+
+    df = DeepFace.find(img_path = r"./tmp/image.jpg", db_path = image_path, distance_metric = metrics[0], model_name = models[1], enforce_detection = False)
+
+    # borrar imagenes de tmp folder
+    for filename in os.listdir(IMAGEDIR):
+        file_path = os.path.join(IMAGEDIR, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+    try:
+        if(not df.empty):
+            print(df.head())
+            if df['Facenet_cosine'][0] < 0.4:     
+                db = get_db(company_code)
+
+                marcaciones = db["marcaciones"]
+
+                collaborator = { "employee_code": employee_code}
+                current_date = datetime.datetime.now()
+                marcacion = { "$push": { "marcaciones": {"latitude": latitude, "longitude": longitude, "date": current_date} } }
+                marcaciones.update_one(collaborator, marcacion)
+
+                return {"access": True, "status": "found, face recognized"}
+            else:
+                return {"access": False, "status": "found, face not recognized"}
+        else:
+            return {"access": False, "status": "no similar faces found"}
+        
+    except Exception as e:
+        return(e)
+
+
+@app.post('/leer_marcaciones')
+async def leer_marcaciones(company_code, employee_code):
+
+    db = get_db(company_code)
+    marcaciones = db["marcaciones"]
+    collaborator = { "employee_code": employee_code}
+    x = marcaciones.find_one(collaborator)
+
+    lista_marcaciones = []
+
+    for i in x["marcaciones"]:
+        print(i)
+        print("latitude: " + str(i["latitude"]) + " longitude: " + str(i["longitude"]) + " date: " + str(i["date"]))
+        item = {"latitude": str(i["latitude"]), "longitude": str(i["longitude"]), "date": str(i["date"]) }
+        lista_marcaciones.append(item)
+
+
+    return{"marcaciones": lista_marcaciones}
+
 
 # iniciar servidor
 if __name__ == '__main__':
