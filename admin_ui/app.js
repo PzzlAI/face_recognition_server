860f66f2c1b75549f70409a4ac1e4cac829d659b
ratatosck <45779848+ratatosck@users.noreply.github.com>
2022-05-26 19:22:42 -0500

    Update app.js

--- admin_ui/app.js
+++ admin_ui/app.js
@@ -11,32 +11,10 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.use(cors())
 
 
-//login
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + "/views/login.html")
-// })
-
 //pagina principal
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/dashboard-pagina-principal.html")
 })
-
-// crear admin
-app.get('/create_admin', (req, res) => {
-  res.send("create_admins");
-})
-
-// lista de admins
-app.get('/get_admins', (req, res) => {
-  res.send("get_admins");
-})
-
-// lista de colaboradores
-app.get('/get_colaborators', (req, res) => {
-  res.send("get_admins");
-})
-
-
 
 app.listen(4000, () => {
   console.log('listening for requests on port 4000')
